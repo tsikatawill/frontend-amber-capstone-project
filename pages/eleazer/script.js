1229150762021b122c7d97e60f25f24ef79ffa5e
abbey eleazer <abbeyelazer@gmail.com>
2023-11-27 15:06:03 +0200

    my page

---
+++ pages/eleazer/script.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      *******    RANDOM JOKE GENERATOR       ********
+
+
+
+const jokeContainer = document.getElementById('joke');
+const btn = document.getElementById('btn');
+
+const getJoke = async () => {
+     jokeContainer.classList.remove('fade')
+
+     const response = await fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,sexist,explicit&type=single&idRange=0-300');
+     const data = await response.json();
+      
+     jokeContainer.classList.add('fade');
+     
+      return jokeContainer.innerHTML = `${data.joke}`;
+
+};
+
+btn.addEventListener('click', getJoke);
